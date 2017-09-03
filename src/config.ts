b7fc86a8bc03dcc053998a6fe2451b51e53851b5
aleks-webby <aleks-webby@yandex.ru>
2017-09-03 21:46:53 +0300

    Add config

---
+++ src/config.ts
@@ -0,0 +1,6 @@
+export const config = {
+  appId: 'd1148e4efc9bfbde0e2aa799e83a26ba',
+  IpInfo: 'https://ipinfo.io/json',
+  currentWeather: 'http://api.openweathermap.org/data/2.5/weather?units=metric',
+  weatherForecast: 'http://api.openweathermap.org/data/2.5/forecast?units=metric'
+};
--- src/services/api/api.service.ts
+++ src/services/api/api.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { Http } from '@angular/http';
+import { config } from '../../config';
 import 'rxjs/add/operator/map';
 
 @Injectable()
@@ -9,21 +10,21 @@ export class ApiService {
   constructor(private http: Http) { }
 
   getCurrentCityName(): Observable<any> {
-    return this.http.get('https://ipinfo.io/json')
+    return this.http.get(config.IpInfo)
       .map(res => {
         return res.json();
       });
   }
 
   getCurrentWeatherInfo(cityName) {
-    return this.http.get('http://api.openweathermap.org/data/2.5/weather?units=metric&APPID=d1148e4efc9bfbde0e2aa799e83a26ba&q=' + cityName)
+    return this.http.get(config.currentWeather + '&APPID=' + config.appId + '&q=' + cityName)
       .map(res => {
         return res.json();
       });
   }
 
   getWeatherForecastInfo(cityName) {
-    return this.http.get('http://api.openweathermap.org/data/2.5/forecast?units=metric&APPID=d1148e4efc9bfbde0e2aa799e83a26ba&q=' + cityName)
+    return this.http.get(config.weatherForecast + '&APPID=' + config.appId + '&q=' + cityName)
       .map(res => {
         return res.json();
       });
